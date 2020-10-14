f2f71a81f19c455cef98e2c5874f6727bfdffc63
lobykin <72708763+lobykin@users.noreply.github.com>
2020-10-15 00:58:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.13.4"
   backend "s3" {
-    bucket         = "nginx-dynamo-bucket-888"
+    bucket         = "nginx-dynamo-bucket-88"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "nginx-locks-2"
