183b38a041138eb5f602ddbfaa8ef3b9c8524550
lobykin <72708763+lobykin@users.noreply.github.com>
2020-10-15 01:28:48 +0200

    Update grafana-install.tf

--- grafana-install.tf
+++ grafana-install.tf
@@ -36,10 +36,10 @@ resource "aws_instance" "grafana-instance" {
     inline = [
       "sudo apt-get remove docker docker-engine docker.io containerd runc -y",
       "sudo apt-get update",
-      "sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y",
+      "sudo apt install apt-transport-https ca-certificates curl software-properties-common -y",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
       "sudo apt-key fingerprint 0EBFCD88",
-      "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu artful stable'",
+      "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'",
       "sudo apt-get update",
       "sudo apt-get install docker-ce docker-ce-cli containerd.io",
       "sudo docker run hello-world",
