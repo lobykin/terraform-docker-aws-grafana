4bb6e8fb9fd9ee9426c1ddf0c75b45beba2ee96f
lobykin <72708763+lobykin@users.noreply.github.com>
2020-10-15 19:58:39 +0200

    Update grafana-install.tf

--- grafana-install.tf
+++ grafana-install.tf
@@ -45,6 +45,7 @@ resource "aws_instance" "grafana-instance" {
       "sudo curl -L 'https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose",
+      "sudo apt-get install docker-compose -y",
       "sudo docker run hello-world",
       "sudo docker network create monitoring",
       "sudo docker volume create grafana-volume",
